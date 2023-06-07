8eba1e94d588f0021e20396426c9d0faa5020352
Devfrank94 <francescomurro94@gmail.com>
2023-06-07 18:08:13 +0200

    add home, main page and first connection controll.

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layout.main')
+
+@section('content')
+  <main>
+    <div class="container py-5 text-center">
+      <h1>{{$title}}</h1>
+      <p>{{$text}}</p>
+    </div>
+  </main>
+@endsection
